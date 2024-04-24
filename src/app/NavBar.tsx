728e01d55de272519e7c33e8e8dd2861e74f7887
oswin-jerome <oswinjeromej@gmail.com>
2024-04-25 01:09:23 +0530

    Hero Section

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -4,9 +4,11 @@ import React from "react";
 
 export default function NavBar() {
   return (
-    <nav className="bg-zinc-100">
+    <nav className="bg-zinc-100 fixed left-0 right-0 z-40">
       <div className="container py-4 flex justify-between md:justify-normal gap-6  md:gap-12 items-center">
-        <div className="font-bold text-lg">Auction4Good</div>
+        <div className="font-bold text-lg">
+          Auction<span className="text-xl text-blue-700">4</span>Good
+        </div>
         <ul className="hidden md:flex gap-10 flex-1">
           <li className="text-zinc-500 hover:text-zinc-800">
             <Link href={"/"}>Home</Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
 import Image from "next/image";
-
+import hero from "@/assets/hero.jpg";
+import { Button } from "@radix-ui/themes";
 export default function Home() {
   return (
-    <div className="container">
-      <h1>Auctions for a cause</h1>
+    <div className="container relative py-4 pt-28">
+      <h1 className="font-bold font-serif text-6xl text-center">Auctions for a cause</h1>
+      <p className="text-center mt-4 text-zinc-500 max-w-[700px] mx-auto">
+        Experience the thrill of giving while making a lasting impact on the causes you care about. Join us on our mission to transform generosity into tangible change, one bid at a time. Together, let's amplify compassion, empower communities, and create a brighter future for all."
+      </p>
+
+      <div className="flex justify-center mt-8">
+        <Button size="4" variant="outline" className="cursor-pointer">
+          Get Started
+        </Button>
+      </div>
+
+      <Image alt="Hero Image" src={hero} quality={100} className="w-[90vw] lg:w-[70vw] mx-auto rounded-xl mt-16 lg:mt-24 border-4 shadow-2xl border-white aspect-[4/3] lg:aspect-[4/2] object-cover" />
     </div>
   );
 }
