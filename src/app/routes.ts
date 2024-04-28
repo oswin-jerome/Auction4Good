7c661cdcf0ee5169facb80688d688c70aedded48
oswin-jerome <oswinjeromej@gmail.com>
2024-04-28 20:42:26 +0530

    Fix route protection

---
+++ src/app/routes.ts
@@ -0,0 +1,14 @@
+/**
+ * List of all routes
+ */
+
+export const HOME = "/";
+export const AUCTIONS = "/auctions";
+export const LOGIN = "/login";
+export const REGISTER = "/register";
+export const AUTH_API = "/api/auth";
+
+/**
+ * List of routes which can be accessed public
+ */
+export const PUBLIC_ROUTES = [HOME, AUCTIONS, LOGIN, REGISTER, AUTH_API];
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,17 +4,46 @@ import type { NextRequest } from "next/server";
 import { redirect } from "next/navigation";
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
+import { AUTH_API, PUBLIC_ROUTES } from "./app/routes";
 export const { auth } = NextAuth(authConfig);
 
 export default async function middleware(req: NextRequest) {
   const { nextUrl } = req;
+
   const user = await auth();
-  if (!user) {
+  const isAuthApiRoute = nextUrl.pathname.startsWith(AUTH_API);
+  const isPublicRoute = PUBLIC_ROUTES.includes(nextUrl.pathname);
+
+  /**
+   * Do nothing if it is a Auth API route
+   */
+  if (isAuthApiRoute) {
+    return null;
+  }
+
+  /**
+   * Redirect to home if user is logged in and accessing login or register page
+   */
+  if ((nextUrl.pathname == "/login" || nextUrl.pathname == "/register") && user?.user) {
+    return Response.redirect(new URL("/", nextUrl));
+  }
+
+  /**
+   * Do nothing if it is a public route
+   */
+  if (isPublicRoute) {
+    return null;
+  }
+
+  /**
+   * Redirect if not logged in
+   */
+  if (user?.user == null) {
     return Response.redirect(new URL("/login", nextUrl));
   }
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: "/host/:path*",
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
